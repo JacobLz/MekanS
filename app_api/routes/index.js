cdc484be2b3384db05e0400e881a188b0694c052
Yakup Aydın <jacob.ligfira@gmail.com>
2026-01-01 18:48:47 +0300

    Update index.js

---
+++ app_api/routes/index.js
@@ -0,0 +1,17 @@
+// yönlendirmeler burada . 
+
+var express = require('express');
+var router = express.Router(); // Yeni bir yönlendirici oluştur
+
+// TEST ROTASI
+// '/venues' adresine GET isteği atılırsa
+router.get('/venues', function(req, res) {
+    // Ekrana JSON formatında cevap bas
+    res.status(200).json({
+        "durum": "Başarili",
+        "mesaj": "MekanS projesi çalişiyor!"
+    });
+});
+
+// Bu satır olmayınca app.js bu dosyayı okumaz dikat !!! (bu kısmada tekrar bak)
+module.exports = router;
