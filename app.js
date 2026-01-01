7c80343bb8c7e08826934fe657917b354e901bec
Yakup Aydın <jacob.ligfira@gmail.com>
2026-01-01 18:31:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,4 +14,12 @@ var app = express();
 
 // ayarlar 
 
+app.use(logger('dev')); /* logger */
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(cors());
+
 
